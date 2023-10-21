8be2ef50e9b0de51aabc3034e2515e524d8ea52f
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2023-10-21 14:40:16 +0200

    List pull requests

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
 // https://docs.github.com/en/actions/security-guides/automatic-token-authentication
 
-use std::env;
+use std::{env, error::Error};
+use octocrab::{self, params};
 
-fn main() {
-    for (k, v) in env::vars() {
-        println!("{k} → {v}");
+fn get_env(env: impl AsRef<str>) -> Option<String> {
+    let env = env.as_ref();
+    env::vars().find_map(|(k, v)| {
+        if k == env { Some(v) } else { None }})
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>>  {
+    // for (k, v) in env::vars() {
+    //     println!("{k} → {v}");
+    // }
+
+    let octocrab = octocrab::instance();
+    let repo = get_env("GITHUB_REPOSITORY")
+        .ok_or("Github repository".to_string())?;
+    let mut repo = repo.split("/");
+    let name = repo.next().ok_or("Repository owner")?;
+    let repo = repo.next().ok_or("Repository name")?;
+    let pulls = octocrab.pulls(name, repo)
+        .list()
+        .state(params::State::Open)
+        .send()
+        .await?;
+
+    for p in pulls {
+        println!("- URL: {}", p.url)
     }
 
-    let event = env::vars().find_map(|(k, v)| {
-        if k == "GITHUB_EVENT_NAME" { Some(v) } else { None }});
-    if let Some(event) = event {
+    if let Some(event) = get_env("GITHUB_EVENT_NAME") {
         if event == "push" {
             println!("This is a push");
         } else {
             println!("Event: {event:?}");
         }
     }
+    Ok(())
 }
