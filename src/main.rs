bd2c1810d08263e374294db3964ef1460377aba8
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2023-10-21 13:54:19 +0200

    Check for the event name

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+// https://docs.github.com/en/actions/security-guides/automatic-token-authentication
+
 use std::env;
 
 fn main() {
     for (k, v) in env::vars() {
         println!("{k} → {v}");
+    }
+
+    let event = env::vars().find_map(|(k, v)| {
+        if k == "GITHUB_EVENT_NAME" { Some(v) } else { None }});
+    if let Some(event) = event {
+        if event == "push" {
+            println!("This is a push");
+        } else {
+            println!("Event: {event:?}");
+        }
     }
 }
